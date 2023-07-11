4a79ab0de4444a1b4884c43afd70dcab5ebff7e2
Douglas Hellberg <DougHellberg@gmail.com>
2023-07-11 09:59:31 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function  saturdayFun(funtime = "roller-skate") {
+    return (`This Saturday, I want to ${funtime}!`);
+}
+
+function mondayWork(workL = "go to the office"){
+    return(`This Monday, I will ${workL}.`);
+}
+function wrapAdjective(p1 = "*"){
+   return function result(p2 = "special"){
+    return(`You are ${p1}${p2}${p1}!`)
+   }
+}
